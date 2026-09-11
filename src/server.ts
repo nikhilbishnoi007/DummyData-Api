7524b6bc667fb44b4d917771aadd542446c92259
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-11 16:23:57 +0530

    change dev script

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from "./app"
 
+
 const PORT = 5000
 
 
 
 app.listen(PORT, () => {
-    console.log(`server is runnint at port: ${PORT}`)
+    console.log(`server is running at port: ${PORT}`)
 })
